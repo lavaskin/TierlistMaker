ff6c1238e5704211aa1f7bf697b0a49ab8dafdaa
Jackson E <lavaskin@gmail.com>
2025-07-16 21:16:44 -0700

    add tierlist tile component

---
+++ src/app/components/tile.component/tile.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { TooltipModule } from 'primeng/tooltip';
+
+@Component({
+	selector: 'app-tile',
+	imports: [TooltipModule],
+	templateUrl: './tile.component.html',
+	styleUrl: './tile.component.scss'
+})
+export class TileComponent {
+	@Input() public title?: string;
+	@Input() public image?: string;
+	@Input() public clickAction?: () => void;
+
+	public onClick() {
+		if (this.clickAction) {
+			this.clickAction();
+		}
+	}
+}
