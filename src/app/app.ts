b0729fc900d798a0ac947ec3d3d301e610a1dbb7
Jackson E <lavaskin@gmail.com>
2025-07-16 22:30:15 -0700

    add better handling for navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterModule],
+	imports: [
+		RouterModule,
+		NavComponent,
+	],
 	templateUrl: './app.html',
 	styleUrl: './app.scss'
 })
