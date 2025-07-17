85cf5ce345cfc52274c1031455bbb7ae91973d62
Jackson E <lavaskin@gmail.com>
2025-07-16 21:21:39 -0700

    add route for viewer page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home/home.page';
 import { TierlistPage } from './pages/tierlist/tierlist.page';
+import { ViewerPage } from './pages/viewer/viewer.page';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: '/home', pathMatch: 'full' },
 	{ path: 'home', component: HomePage },
 	{ path: 'tierlist/:id', component: TierlistPage },
+	{ path: 'viewer/:id', component: ViewerPage },
 
 	{ path: '**', redirectTo: '/home' }
 ];
