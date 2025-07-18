bd36c1d8361708fa45c5dd5da7ac0621d2e1970f
Jackson E <lavaskin@gmail.com>
2025-07-18 01:17:41 -0700

    add link to creation of tierlist from nav bar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
 	selector: 'app-nav',
-	imports: [CommonModule, RouterModule],
+	imports: [
+		CommonModule, RouterModule,
+		ButtonModule,
+	],
 	templateUrl: './nav.component.html',
 	styleUrl: './nav.component.scss'
 })
