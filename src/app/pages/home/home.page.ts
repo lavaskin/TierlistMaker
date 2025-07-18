c122501b4cf8413782255f59b7b02c85b271e45d
Jackson E <lavaskin@gmail.com>
2025-07-17 22:02:28 -0700

    update home to show user tierlists and templates

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,24 +1,49 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { SpinnerComponent } from '@app/components/spinner/spinner.component';
 import { TierlistIds } from '@app/models/enums/tierlist-ids.enum';
+import { TierlistModel } from '@app/models/tierlist.model';
+import { StorageService } from '@app/services/storage.service';
+import { TierlistService } from '@app/services/tierlist.service';
 import { ButtonModule } from 'primeng/button';
+import { forkJoin } from 'rxjs';
 
 @Component({
 	selector: 'page-home',
 	imports: [
 		CommonModule, RouterModule,
-		ButtonModule,
+		SpinnerComponent, ButtonModule,
 	],
 	templateUrl: './home.page.html',
 	styleUrl: './home.page.scss'
 })
 export class HomePage {
-	private _router = inject(Router);
+	public isLoading: boolean = false;
+	public tierlistTemplates: TierlistModel[] = [];
+	public userTierlists: TierlistModel[] = [];
 
-	public tierlists = [
-		{ id: TierlistIds.BlueArchive, name: "Blue Archive" },
-	];
+	private _router = inject(Router);
+	private _tierlists = inject(TierlistService);
+	private _storage = inject(StorageService);
+
+	ngOnInit(): void {
+		this.isLoading = true;
+
+		// Fetch both user tierlists and all the available templates
+		forkJoin({
+			tierlistTemplates: this._tierlists.getAllTierlists(),
+			userTierlists: this._storage.getAllTierlists()
+		}).subscribe({
+			next: ({ tierlistTemplates, userTierlists }) => {
+				this.tierlistTemplates = tierlistTemplates;
+				this.userTierlists = userTierlists;
+			},
+			error: (err) => {
+				console.error('Error fetching data:', err);
+			}
+		}).add(() => this.isLoading = false);
+	}
 
 	public navigateToTierlist(tierlistId: TierlistIds) {
 		this._router.navigate(['/tierlist', tierlistId]);
