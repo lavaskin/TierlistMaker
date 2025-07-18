410ed1e3980fbf694b6f36dac07e6238a55dbb7a
Jackson E <lavaskin@gmail.com>
2025-07-17 21:09:37 -0700

    add buttons for tierlist templates on home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { TierlistIds } from '@app/models/enums/tierlist-ids.enum';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
 	selector: 'page-home',
-	imports: [],
+	imports: [
+		CommonModule, RouterModule,
+		ButtonModule,
+	],
 	templateUrl: './home.page.html',
 	styleUrl: './home.page.scss'
 })
-export class HomePage { }
+export class HomePage {
+	private _router = inject(Router);
+
+	public tierlists = [
+		{ id: TierlistIds.BlueArchive, name: "Blue Archive" },
+	];
+
+	public navigateToTierlist(tierlistId: TierlistIds) {
+		this._router.navigate(['/tierlist', tierlistId]);
+	}
+}
