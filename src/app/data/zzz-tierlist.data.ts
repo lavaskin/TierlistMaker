f72ca6a45158ef28b096db7323ce6c2273a4151b
Jackson E <lavaskin@gmail.com>
2025-07-19 15:59:55 -0700

    more zzz info

--- src/app/data/zzz-tierlist.data.ts
+++ src/app/data/zzz-tierlist.data.ts
@@ -8,35 +8,35 @@ const zzzTierlistData: TierlistModel = {
 	name: "Zenless Zone Zero",
 	items: [
 		{
-			name: 'Alice',
+			name: 'Alice Thymefield',
 			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/7/7e/Agent_Alice_Thymefield_Icon.png',
 			variations: [
 				{ name: 'Default', image: '', },
 			],
 		},
 		{
-			name: 'Rina',
+			name: '	Alexandrina Sebastiane',
 			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/a/a8/Agent_Alexandrina_Sebastiane_Icon.png',
 			variations: [
 				{ name: 'Default', image: '', },
 			],
 		},
 		{
-			name: 'Anby',
+			name: '	Anby Demara',
 			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/b/b1/Agent_Anby_Demara_Icon.png',
 			variations: [
 				{ name: 'Default', image: '', },
 			],
 		},
 		{
-			name: 'Anton',
+			name: 'Anton Ivanov',
 			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/1/13/Agent_Anton_Ivanov_Icon.png',
 			variations: [
 				{ name: 'Default', image: '', },
 			],
 		},
 		{
-			name: 'Asaba',
+			name: '	Asaba Harumasa',
 			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/fe/Agent_Asaba_Harumasa_Icon.png',
 			variations: [
 				{ name: 'Default', image: '', },
@@ -50,6 +50,13 @@ const zzzTierlistData: TierlistModel = {
 			],
 		},
 		{
+			name: 'Belle',
+			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/e/ee/Agent_Belle_Icon.png',
+			variations: [
+				{ name: 'Default', image: '', },
+			],
+		},
+		{
 			name: 'Ben Bigger',
 			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/b/ba/Agent_Ben_Bigger_Icon.png',
 			variations: [
@@ -57,29 +64,36 @@ const zzzTierlistData: TierlistModel = {
 			],
 		},
 		{
-			name: 'Billy',
+			name: '	Billy Kid',
 			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/9/99/Agent_Billy_Kid_Icon.png',
 			variations: [
 				{ name: 'Default', image: '', },
 			],
 		},
 		{
-			name: 'Burnice',
+			name: '	Burnice White',
 			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f9/Agent_Burnice_White_Icon.png',
 			variations: [
 				{ name: 'Default', image: '', },
 			],
 		},
 		{
-			name: 'Caeser',
+			name: 'Caesar King',
 			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/a/af/Agent_Caesar_King_Icon.png',
 			variations: [
 				{ name: 'Default', image: '', },
 			],
 		},
 		{
-			name: 'Corin',
+			name: 'Corin Wickes',
 			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/e/e4/Agent_Corin_Wickes_Icon.png',
+			variations: [
+				{ name: 'Default', image: '', },
+			],
+		},
+		{
+			name: 'Ellen Joe',
+			thumbnail: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/e/ed/Agent_Ellen_Joe_Icon.png',
 			variations: [
 				{ name: 'Default', image: '', },
 			],
