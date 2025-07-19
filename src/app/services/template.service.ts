c5207f9e1d40aafaf2af62254dea3d42cd4e4482
Jackson E <lavaskin@gmail.com>
2025-07-18 20:47:31 -0700

    add default tiers on template service when initializing

--- src/app/services/template.service.ts
+++ src/app/services/template.service.ts
@@ -11,6 +11,15 @@ export class TemplateService {
 	// Deep copy the tierlists data to avoid mutation
 	private _templates: TierlistModel[] = deepCopy(TierlistsData);
 
+	private _defaultTiers = [
+		{ label: 'S', color: '#ff0000' },
+		{ label: 'A', color: '#f15b4c' },
+		{ label: 'B', color: '#ef5b70' },
+		{ label: 'C', color: '#d35d90' },
+		{ label: 'D', color: '#9b6b9c' },
+		{ label: 'F', color: '#c39ec9' },
+	];
+
 	public get(templateId: number): Observable<TierlistModel> {
 		const tierlist = this._templates.find(t => t.templateId == templateId);
 		if (!tierlist) {
@@ -19,14 +28,16 @@ export class TemplateService {
 
 		// Copy the default tiers onto the user tiers, and init the tierlist items1
 		const tierlistCopy = deepCopy(tierlist);
-		if (tierlistCopy.defaultTiers) {
-			tierlistCopy.tiers = deepCopy(tierlistCopy.defaultTiers);
-
-			// Add empty array for items
-			tierlistCopy.tiers.forEach((tier: any) => {
-				tier.items = [];
-			});
+		console.log("TemplateService.get", tierlistCopy);
+		if (!tierlistCopy.defaultTiers) {
+			tierlistCopy.defaultTiers = this._defaultTiers;
 		}
+
+		// Add empty array for items and copy default tiers to user tiers for modification
+		tierlistCopy.tiers = deepCopy(tierlistCopy.defaultTiers);
+		tierlistCopy.tiers.forEach((tier: any) => {
+			tier.items = [];
+		});
 
 		// Return the tierlist
 		return new Observable<TierlistModel>((observer) => {
