e0dc6b18494e034acedf09074950ad95c5b89a05
Jackson E <lavaskin@gmail.com>
2025-07-23 17:03:11 -0700

    add util function for template pre-processing

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,3 +1,19 @@
+import { TierlistModel } from "./models/tierlist.model";
+
 export function deepCopy(obj: any): any {
 	return JSON.parse(JSON.stringify(obj));
+}
+
+export function preProcessTemplateData(templateData: TierlistModel, sort: boolean = true): any {
+	// Add an id to all items
+	templateData.items.forEach((item: any, index: number) => {
+		item.id = index + 1; // Assigning a unique id starting from 1
+	});
+
+	// Sort the items by name
+	if (sort) {
+		templateData.items.sort((a: any, b: any) => a.name.localeCompare(b.name));
+	}
+
+	return templateData;
 }
