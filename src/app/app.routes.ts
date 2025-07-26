e1fc017f108ed7dd64178fa2e9675316661725db
Jackson E <lavaskin@gmail.com>
2025-07-25 19:34:46 -0700

    add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePage } from './pages/home/home.page';
 import { TierlistPage } from './pages/tierlist/tierlist.page';
 import { ViewerPage } from './pages/viewer/viewer.page';
 import { CreatePage } from './pages/create/create.page';
+import { LoginPage } from './pages/login/login.page';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: '/home', pathMatch: 'full' },
 	{ path: 'home', component: HomePage },
+	{ path: 'login', component: LoginPage },
 	{ path: 'create', component: CreatePage },
 	{ path: 'create/:id', component: CreatePage },
 	{ path: 'tierlist/:id', component: TierlistPage },
